8ce548354ff117e62ede06b2bbae2438aad5e617
Marcela1995 <55165743+Marcela1995@users.noreply.github.com>
2023-05-09 19:45:00 -0500

    Update TiposDatos.java
    Actualizado-imprime

--- EjercicioTemaUno/src/ejercicio/tema/uno/TiposDatos.java
+++ EjercicioTemaUno/src/ejercicio/tema/uno/TiposDatos.java
@@ -16,22 +16,32 @@ public class TiposDatos {
 
         //ENTEROS
         int variable0  = 12456;
+        System.out.println(variable0);
         byte variable1 = 4;
+         System.out.println(variable1);
         short variable2= 10;
+        System.out.println(variable2);
         long variable3 = 100;
+        System.out.println(variable3);
 
 
         //DECIMALES
         float variable4  = 10.5f;
+        System.out.println(variable4);
         double variable5 = 10.5d;
+        System.out.println(variable5);
 
         //BOOLEAN
         boolean variable6 = true;
+        System.out.println(variable6);
         boolean variable7 = true;
+        System.out.println(variable7);
 
         //TEXTOS
         String variable8 = "Tipos de dato String";
+        System.out.println(variable8);
         char variable9 = 'C';
+        System.out.println(variable9);
 
     }
 
