9e00f8a80e6c430c102bdfa3c5810a36e0a2d902
Marcela1995 <marce.zhag@gmail.com>
2023-05-09 19:14:59 -0500

    Tipos de datos

---
+++ EjercicioTemaUno/src/ejercicio/tema/uno/TiposDatos.java
@@ -0,0 +1,41 @@
+package ejercicio.tema.uno;
+
+/**
+ * @author Zhagüi
+ */
+public class TiposDatos {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        /**
+         * TIPOS DE DATOS
+         */
+
+
+        //ENTEROS
+        int variable0  = 12456;
+        byte variable1 = 4;
+        short variable2= 10;
+        long variable3 = 100;
+
+
+        //DECIMALES
+        float variable4  = 10.5f;
+        double variable5 = 10.5d;
+
+        //BOOLEAN
+        boolean variable6 = true;
+        boolean variable7 = true;
+
+        //TEXTOS
+        String variable8 = "Tipos de dato String";
+        char variable9 = 'C';
+
+    }
+
+
+
+
+}
